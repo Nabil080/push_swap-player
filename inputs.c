286dcb2f4c142768a26f7b0afba3ac4dcd6b3c80
Nabil080 <nbellila@student.42.fr>
2024-06-12 19:35:11 +0200

    cleaner inputs fonctions

--- inputs.c
+++ inputs.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 14:20:21 by nbellila          #+#    #+#             */
-/*   Updated: 2024/06/12 19:00:09 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/06/12 19:34:33 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,49 +15,44 @@
 static void	show_options(char *buffer)
 {
 	ft_printf("Que souhaitez vous faire ?\n\n");
-	ft_printf("0 : Terminer le programme\n");
-	ft_printf("1 : Voir les piles\n");
-	ft_printf("2 : sa (swap a)\n");
-	ft_printf("3 : sb (swap b)\n");
-	ft_printf("4 : ss (swap a & b)\n");
-	ft_printf("5 : pa (push a)\n");
-	ft_printf("6 : pb (push b)\n");
-	ft_printf("7 : ra (rotate a)\n");
-	ft_printf("8 : rb (rotate b)\n");
-	ft_printf("9 : rr (rotate a & b)\n");
-	ft_printf("a : rra (reverse rotate a)\n");
-	ft_printf("b : rrb (reverse rotate b)\n");
+	ft_printf("0 : Voir les piles\n");
+	ft_printf("1 : sa (swap a)\n");
+	ft_printf("2 : sb (swap b)\n");
+	ft_printf("3 : ss (swap a & b)\n");
+	ft_printf("4 : pa (push a)\n");
+	ft_printf("5 : pb (push b)\n");
+	ft_printf("6 : ra (rotate a)\n");
+	ft_printf("7 : rb (rotate b)\n");
+	ft_printf("8 : rr (rotate a & b)\n");
+	ft_printf("9 : rra (reverse rotate a)\n");
+	ft_printf("10 : rrb (reverse rotate b)\n");
+	ft_printf("11 : rrr (reverse rotate a & b)\n");
+	ft_printf("q : Terminer le programme\n");
 	read(0, buffer, 1);
 }
 
 void	get_user_input(t_list **a, t_list **b)
 {
-	char	buffer;
+	char			buffer;
+	long			index;
+	t_operations	operations[12];
 
+	operations[0] = &show_stacks;
+	operations[1] = &sa;
+	operations[2] = &sb;
+	operations[3] = &ss;
+	operations[4] = &pa;
+	operations[5] = &pb;
+	operations[6] = &ra;
+	operations[7] = &rb;
+	operations[8] = &rr;
+	operations[9] = &rra;
+	operations[10] = &rrb;
+	operations[11] = &rrr;
 	show_options(&buffer);
-	if (buffer == '0')
+	if (buffer == 'q')
 		return ;
-	if (buffer == '1')
-		show_stacks(a, b);
-	if (buffer == '2')
-		sa(a, b);
-	if (buffer == '3')
-		sb(a, b);
-	if (buffer == '4')
-		ss(a, b);
-	if (buffer == '5')
-		pa(a, b);
-	if (buffer == '6')
-		pb(a, b);
-	if (buffer == '7')
-		ra(a, b);
-	if (buffer == '8')
-		rb(a, b);
-	if (buffer == '9')
-		rr(a, b);
-	if (buffer == 'a')
-		rra(a, b);
-	if (buffer == 'b')
-		rrb(a, b);
+	index = ft_atoi(&buffer);
+	operations[index](a, b);
 	get_user_input(a, b);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 17:17:03 by nbellila          #+#    #+#             */
-/*   Updated: 2024/06/12 19:02:29 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/06/12 19:34:58 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ void	exit_success(t_list	*a);
 /*
 * operations
 */
+typedef void	(*t_operations)(t_list **, t_list **);
+
 void	swap_top(t_list **stack);
 
 void	sa(t_list **a, t_list **b);
