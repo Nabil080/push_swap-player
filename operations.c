0ee43b20c9bce9bf55190691718e821a921e1f41
Nabil080 <nbellila@student.42.fr>
2024-06-12 16:58:54 +0200

    push top proto

--- operations.c
+++ operations.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 13:31:04 by nbellila          #+#    #+#             */
-/*   Updated: 2024/06/12 16:58:36 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/06/12 16:58:48 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,7 @@ void	pa(t_list *a, t_list *b)
 {
 	push_top(b, a);
 	ft_printf("Operation done : pa\n");
-	show_stacks(a, b);	ft_printf("")
-
+	show_stacks(a, b);
 }
 
 void	pb(t_list *a, t_list *b)
