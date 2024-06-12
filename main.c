48a7250c297afa5259d8752a1246900755d81b68
Nabil080 <nbellila@student.42.fr>
2024-06-12 13:39:12 +0200

    show stack starting from top

--- main.c
+++ main.c
@@ -6,31 +6,31 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 17:19:49 by nbellila          #+#    #+#             */
-/*   Updated: 2024/06/11 13:54:18 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/06/12 13:39:00 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	show_lst(t_list *lst)
+static void	show_stack(t_list *lst)
 {
-	while (lst)
-	{
-		ft_printf("%d\n", *(int *)lst->content);
-		lst = lst->next;
-	}
+	if (lst->next)
+		show_lst(lst->next);
+	ft_printf("%d\n", *(int *)lst->content);
 }
 
 int main(int argc, char *argv[])
 {
 	t_list	*a;
-	//t_list	*b;
+	t_list	*b;
+
 	if (!args_check(argc, argv))
 		return (1);
 	a = parse_as_lst(argc, argv);
 	if (!a)
 		return (1);
-	show_lst(a);
+	b = NULL;
+	show_stack(a);
 	ft_lstclear(&a, free);
 	return (0);
 }
